7f74073c78df619548e6ffcc106c163c90c32cd5
azbo400 <azbo400@gmail.com>
2017-03-24 10:20:48 -0700

    removed body parser

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ var app = express();
 
 var multer = require('multer');
 var upload = multer({dest:'uploads/'});
-var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
 
 app.set('view engine','ejs');
-app.use(bodyParser.json());
 
 app.get('/',function(req,res){
   res.render('index');
