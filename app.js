5872e6a8992b453e8baa4a042998ba3b6822bbb3
azbo400 <azbo400@gmail.com>
2017-03-27 15:57:43 -0700

    Fixed it not returning anything in the JSON object

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer({dest:'uploads/'});
 
 var port = process.env.PORT || 3000;
 
 app.set('view engine','ejs');
+app.use(bodyParser.json());
 
 app.get('/',function(req,res){
   res.render('index');
 });
 
-app.post('/',upload.any(),function(req,res,next){
-  res.send({"size":req.size});
+app.post('/upload',upload.single('file'),function(req,res,next){
+  res.send({"size":req.file.size});
 }); 
 
 app.listen(port,function(){
